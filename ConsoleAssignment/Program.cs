e378b03b579c11e02a86f5863ef14a602f344588
Sumith R <sumith.r@nexergroup.com>
2026-03-16 17:15:29 +0530

    C# Console Assignment

---
+++ ConsoleAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Student
+{
+    public string Name;       
+    public int Age;
+    public double Marks;
+
+    public string GradeCalculation()
+    {
+        if (Marks >= 90)
+        {
+            return "A";
+        }
+        else if (Marks >= 75)
+        {
+            return "B";
+        }
+        else if (Marks >= 50)
+        {
+            return "C";
+        }
+        else
+        {
+            return "Fail";
+        }
+    }
+
+    public void Result()
+    {
+        Console.WriteLine($"Student Name: {Name}");   
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Marks: {Marks}");
+        Console.WriteLine($"Grade: {GradeCalculation()}");
+    }
+}
+
+partial class Program
+{
+    static void Main(String[] args)
+    {
+        Student s = new Student();
+
+        Console.Write("Enter Student Name: ");
+        s.Name = Console.ReadLine();              
+
+        Console.Write("Enter Student Age: ");
+        s.Age = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Student Marks: ");
+        s.Marks = double.Parse(Console.ReadLine());  
+
+        Console.WriteLine("");
+        s.Result();
+    }
+}
